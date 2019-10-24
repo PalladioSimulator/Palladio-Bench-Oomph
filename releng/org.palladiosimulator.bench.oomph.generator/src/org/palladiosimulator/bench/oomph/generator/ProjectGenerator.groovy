f41f93903fff5c3b7194ad09515c573d6ab8d519
Sebastian Krach <krach@fzi.de>
2019-10-24 17:24:05 +0200

    Readded explicit processing of dependencies

--- releng/org.palladiosimulator.bench.oomph.generator/src/org/palladiosimulator/bench/oomph/generator/ProjectGenerator.groovy
+++ releng/org.palladiosimulator.bench.oomph.generator/src/org/palladiosimulator/bench/oomph/generator/ProjectGenerator.groovy
@@ -1,5 +1,6 @@
 package org.palladiosimulator.bench.oomph.generator
 
+import org.palladiosimulator.maven.tychotprefresh.tp.model.TargetPlatformFile
 import org.palladiosimulator.maven.tychotprefresh.tp.parser.TargetPlatformParser
 
 import static org.palladiosimulator.bench.oomph.generator.GenerationUtils.writeToFile
@@ -19,14 +20,28 @@ class ProjectGenerator {
     }
 
     protected Map determineDependencies(String targetPlatformURL) {
+        //The following target platforms are temporarily hardcoded
+        //FIXME:It would be more proper to evaluate the dynamic tp generated during the maven build of the project
+        def locations = [retrieveTPFile(targetPlatformURL),
+         retrieveTPFile("https://raw.githubusercontent.com/MDSD-Tools/Maven-Build-TargetPlatforms/master/targetPlatforms/eclipse-modeling-2019-06.target"),
+         retrieveTPFile("https://raw.githubusercontent.com/PalladioSimulator/Palladio-Build-MavenTP/master/license.target")]
+            .findAll {it.isPresent()}
+            .collect {it.get()}
+            .collectMany {it.locations.findAll { it.filter ? it.filter == "nightly" : true }}
+
+
+        [sites: locations.collect { it.repositoryLocation },
+         units: locations.collectMany {
+             it.units.collect { it.id }
+         }]
+
+    }
+
+    private Optional<TargetPlatformFile> retrieveTPFile(String platformURL) {
         File tf = File.createTempFile("targetplatform", ".tmp.target")
-        tf << new URL(targetPlatformURL).getText()
-        TargetPlatformParser.parse(tf).map {
-            def filteredLocations = it.locations.findAll { it.filter ? it.filter == "nightly" : true }
-            [sites: filteredLocations.collect { it.repositoryLocation },
-             units: filteredLocations.collectMany {
-                 it.units.collect { it.id }
-             }]
-        }.orElse([:])
+        tf << new URL(platformURL).getText()
+        def tp = TargetPlatformParser.parse(tf)
+        tf.delete()
+        tp
     }
 }
